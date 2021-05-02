552a7594139109575a7105bf2525bac6ab3372f9
Daphne-Dev <daphned@naver.com>
2021-05-03 00:58:37 +0900

    edit password validation

--- services/UserService.js
+++ services/UserService.js
@@ -13,19 +13,7 @@ const findUser = (conn, field) => {
   return conn.query(`SELECT ${column} FROM users WHERE ${column} = ?`, [field[column]]);
 };
 
-const passwordValidation = (password) => {
-  const pattern1 = /[0-9]/;
-  const pattern2 = /[a-zA-Z]/;
-
-  if (!pattern1.test(password) || !pattern2.test(password) || password.length < 8) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
 export default {
   createUser,
   findUser,
-  passwordValidation,
 };
